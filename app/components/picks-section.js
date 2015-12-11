d269d3eb469714d34bde9124a6af09fa48359bae
Jason Barry <barryofguilder@gmail.com>
2015-12-11 17:10:20 -0500

    Fix where picks background color did not always extend entire column

---
+++ app/components/picks-section.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['picks'],
+
+  didInsertElement() {
+    let element = this.$();
+    this.fixPickHeight(element);
+
+    $(window).resize(() => {
+      this.fixPickHeight(element);
+    });
+  },
+
+  fixPickHeight(element) {
+    // Clearing the height and setting it fixes where the background
+    // color doesn't always extend the element.
+    let row = element.find('.row');
+    row.css('height', '');
+    row.css('height', row.height());
+  }
+});
